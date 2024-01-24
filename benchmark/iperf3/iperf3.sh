709443b930e761209751eaa776212d95f0a8992c
Naoki MATSUMOTO <naoki@pibvt.net>
2024-01-24 18:39:45 +0900

    use iperf-3.16
    Signed-off-by: Naoki MATSUMOTO <naoki@pibvt.net>

--- benchmark/iperf3/iperf3.sh
+++ benchmark/iperf3/iperf3.sh
@@ -4,37 +4,38 @@ set -eu -o pipefail
 
 cd $(dirname $0)
 
-ALPINE_IMAGE="public.ecr.aws/docker/library/alpine:3.16"
-
 source ~/.profile
 . ../param.bash
 
-sudo nerdctl pull --quiet $ALPINE_IMAGE
-nerdctl pull --quiet $ALPINE_IMAGE
+IMAGE_NAME="iperf3"
+# sometimes fail to pull images
+# this is workaround
+# https://github.com/containerd/nerdctl/issues/622
+systemctl --user restart containerd
+sleep 1
+systemctl --user restart buildkit
+sleep 3
+systemctl --user status --no-pager containerd
+systemctl --user status --no-pager buildkit
+
+sudo nerdctl build -f ./Dockerfile -t $IMAGE_NAME .
+nerdctl build -f ./Dockerfile -t $IMAGE_NAME .
 
 echo "===== Benchmark: iperf3 rootful via NetNS ====="
 (
   set +e
   sudo nerdctl rm -f iperf3-server
   sudo nerdctl rm -f iperf3-client
-  systemctl --user stop iperf3-server
   set -ex
 
-  sudo nerdctl run -d --name iperf3-server $ALPINE_IMAGE sleep infinity
-  sudo nerdctl exec iperf3-server apk add --no-cache iperf3
-  sudo nerdctl run -d --name iperf3-client $ALPINE_IMAGE sleep infinity
-  sudo nerdctl exec iperf3-client apk add --no-cache iperf3
-
-  systemd-run --user --unit iperf3-server sudo nerdctl exec iperf3-server iperf3 -s
-
+  sudo nerdctl run -d --name iperf3-server $IMAGE_NAME iperf3 -s
   SERVER_IP=$(sudo nerdctl exec iperf3-server hostname -i)
+
   sleep 1
-  sudo nerdctl exec iperf3-client iperf3 -c $SERVER_IP -i 0 --connect-timeout 1000 -J > iperf3-rootful-direct.log
+  sudo nerdctl run --name iperf3-client $IMAGE_NAME iperf3 -c $SERVER_IP -i 0 --connect-timeout 1000 -J > iperf3-rootful-direct.log
 
   sudo nerdctl rm -f iperf3-server
   sudo nerdctl rm -f iperf3-client
-  systemctl --user stop iperf3-server
-  systemctl --user reset-failed
 )
 
 echo "===== Benchmark: iperf3 rootful via host ====="
@@ -42,24 +43,16 @@ echo "===== Benchmark: iperf3 rootful via host ====="
   set +e
   sudo nerdctl rm -f iperf3-server
   sudo nerdctl rm -f iperf3-client
-  systemctl --user stop iperf3-server
-  systemctl --user reset-failed
   set -ex
 
-  sudo nerdctl run -d --name iperf3-server -p 5202:5201 $ALPINE_IMAGE sleep infinity
-  sudo nerdctl exec iperf3-server apk add --no-cache iperf3
-  sudo nerdctl run -d --name iperf3-client $ALPINE_IMAGE sleep infinity
-  sudo nerdctl exec iperf3-client apk add --no-cache iperf3
-
-  systemd-run --user --unit iperf3-server sudo nerdctl exec iperf3-server iperf3 -s
+  sudo nerdctl run -d --name iperf3-server -p 5202:5201 $IMAGE_NAME iperf3 -s
+  SERVER_IP=$(sudo nerdctl exec iperf3-server hostname -i)
 
   sleep 1
-  sudo nerdctl exec iperf3-client iperf3 -c $HOST_IP -p 5202 -i 0 --connect-timeout 1000 -J > iperf3-rootful-host.log
+  sudo nerdctl run --name iperf3-client $IMAGE_NAME iperf3 -c $HOST_IP -p 5202 -i 0 --connect-timeout 1000 -J > iperf3-rootful-host.log
 
   sudo nerdctl rm -f iperf3-server
   sudo nerdctl rm -f iperf3-client
-  systemctl --user stop iperf3-server
-  systemctl --user reset-failed
 )
 
 echo "===== Benchmark: iperf3 client(w/o bypass4netns) server(w/o bypass4netns) via intermediate NetNS ====="
@@ -67,24 +60,16 @@ echo "===== Benchmark: iperf3 client(w/o bypass4netns) server(w/o bypass4netns) 
   set +e
   nerdctl rm -f iperf3-server
   nerdctl rm -f iperf3-client
-  systemctl --user stop iperf3-server
-  systemctl --user reset-failed
   set -ex
 
-  nerdctl run -d --name iperf3-server $ALPINE_IMAGE sleep infinity
-  nerdctl exec iperf3-server apk add --no-cache iperf3
-  nerdctl run -d --name iperf3-client $ALPINE_IMAGE sleep infinity
-  nerdctl exec iperf3-client apk add --no-cache iperf3
-
-  systemd-run --user --unit iperf3-server nerdctl exec iperf3-server iperf3 -s
-
+  nerdctl run -d --name iperf3-server $IMAGE_NAME iperf3 -s
   SERVER_IP=$(nerdctl exec iperf3-server hostname -i)
+
   sleep 1
-  nerdctl exec iperf3-client iperf3 -c $SERVER_IP -i 0 --connect-timeout 1000 -J > iperf3-wo-b4ns-direct.log
+  nerdctl run --name iperf3-client $IMAGE_NAME iperf3 -c $SERVER_IP -i 0 --connect-timeout 1000 -J > iperf3-wo-b4ns-direct.log
 
   nerdctl rm -f iperf3-server
   nerdctl rm -f iperf3-client
-  systemctl --user stop iperf3-server
   systemctl --user reset-failed
 )
 
@@ -93,24 +78,15 @@ echo "===== Benchmark: iperf3 client(w/o bypass4netns) server(w/o bypass4netns) 
   set +e
   nerdctl rm -f iperf3-server
   nerdctl rm -f iperf3-client
-  systemctl --user stop iperf3-server
-  systemctl --user reset-failed
   set -ex
 
-  nerdctl run -d --name iperf3-server -p 5202:5201 $ALPINE_IMAGE sleep infinity
-  nerdctl exec iperf3-server apk add --no-cache iperf3
-  nerdctl run -d --name iperf3-client $ALPINE_IMAGE sleep infinity
-  nerdctl exec iperf3-client apk add --no-cache iperf3
-
-  systemd-run --user --unit iperf3-server nerdctl exec iperf3-server iperf3 -s
+  nerdctl run -d --name iperf3-server -p 5202:5201 $IMAGE_NAME iperf3 -s
 
   sleep 1
-  nerdctl exec iperf3-client iperf3 -c $HOST_IP -p 5202 -i 0 --connect-timeout 1000 -J > iperf3-wo-b4ns-host.log
+  nerdctl run --name iperf3-client $IMAGE_NAME iperf3 -c $HOST_IP -p 5202 -i 0 --connect-timeout 1000 -J > iperf3-wo-b4ns-host.log
 
   nerdctl rm -f iperf3-server
   nerdctl rm -f iperf3-client
-  systemctl --user stop iperf3-server
-  systemctl --user reset-failed
 )
 
 echo "===== Benchmark: iperf3 client(w/ bypass4netns) server(w/ bypass4netns) via host ====="
@@ -118,26 +94,19 @@ echo "===== Benchmark: iperf3 client(w/ bypass4netns) server(w/ bypass4netns) vi
   set +e
   nerdctl rm -f iperf3-server
   nerdctl rm -f iperf3-client
-  systemctl --user stop iperf3-server
   systemctl --user stop run-bypass4netnsd
   systemctl --user reset-failed
   set -ex
 
   systemd-run --user --unit run-bypass4netnsd bypass4netnsd 
 
-  nerdctl run --label nerdctl/bypass4netns=true -d --name iperf3-server -p 5202:5201 $ALPINE_IMAGE sleep infinity
-  nerdctl exec iperf3-server apk add --no-cache iperf3
-  nerdctl run --label nerdctl/bypass4netns=true -d --name iperf3-client $ALPINE_IMAGE sleep infinity
-  nerdctl exec iperf3-client apk add --no-cache iperf3
-
-  systemd-run --user --unit iperf3-server nerdctl exec iperf3-server iperf3 -s
+  nerdctl run --label nerdctl/bypass4netns=true -d --name iperf3-server -p 5202:5201 $IMAGE_NAME iperf3 -s
 
   sleep 1
-  nerdctl exec iperf3-client iperf3 -c $HOST_IP -p 5202 -i 0 --connect-timeout 1000 -J > iperf3-w-b4ns.log
+  nerdctl run --label nerdctl/bypass4netns=true --name iperf3-client $IMAGE_NAME iperf3 -c $HOST_IP -p 5202 -i 0 --connect-timeout 1000 -J > iperf3-w-b4ns.log
 
   nerdctl rm -f iperf3-server
   nerdctl rm -f iperf3-client
-  systemctl --user stop iperf3-server
   systemctl --user stop run-bypass4netnsd
   systemctl --user reset-failed
 )
@@ -147,7 +116,6 @@ echo "===== Benchmark: iperf3 client(w/ bypass4netns) server(w/ bypass4netns) wi
   set +e
   nerdctl rm -f iperf3-server
   nerdctl rm -f iperf3-client
-  systemctl --user stop iperf3-server
   systemctl --user stop run-bypass4netnsd
   systemctl --user stop etcd.service
   systemctl --user reset-failed
@@ -156,20 +124,14 @@ echo "===== Benchmark: iperf3 client(w/ bypass4netns) server(w/ bypass4netns) wi
   systemd-run --user --unit etcd.service /usr/bin/etcd --listen-client-urls http://$HOST_IP:2379 --advertise-client-urls http://$HOST_IP:2379
   systemd-run --user --unit run-bypass4netnsd bypass4netnsd --multinode=true --multinode-etcd-address=http://$HOST_IP:2379 --multinode-host-address=$HOST_IP
 
-  nerdctl run --label nerdctl/bypass4netns=true -d --name iperf3-server -p 5202:5201 $ALPINE_IMAGE sleep infinity
-  nerdctl exec iperf3-server apk add --no-cache iperf3
-  nerdctl run --label nerdctl/bypass4netns=true -d --name iperf3-client $ALPINE_IMAGE sleep infinity
-  nerdctl exec iperf3-client apk add --no-cache iperf3
-
-  systemd-run --user --unit iperf3-server nerdctl exec iperf3-server iperf3 -s
-
+  nerdctl run --label nerdctl/bypass4netns=true -d --name iperf3-server -p 5202:5201 $IMAGE_NAME iperf3 -s
   SERVER_IP=$(nerdctl exec iperf3-server hostname -i)
+
   sleep 1
-  nerdctl exec iperf3-client iperf3 -c $SERVER_IP -i 0 --connect-timeout 1000
+  nerdctl run --label nerdctl/bypass4netns=true --name iperf3-client $IMAGE_NAME iperf3 -c $SERVER_IP -i 0 --connect-timeout 1000
 
   nerdctl rm -f iperf3-server
   nerdctl rm -f iperf3-client
-  systemctl --user stop iperf3-server
   systemctl --user stop run-bypass4netnsd
   systemctl --user stop etcd.service
   systemctl --user reset-failed
--- benchmark/iperf3/iperf3_multinode.sh
+++ benchmark/iperf3/iperf3_multinode.sh
@@ -12,12 +12,19 @@ TEST1_VXLAN_MAC="02:42:c0:a8:00:1"
 TEST1_VXLAN_ADDR="192.168.2.1"
 TEST2_VXLAN_MAC="02:42:c0:a8:00:2"
 TEST2_VXLAN_ADDR="192.168.2.2"
-ALPINE_IMAGE="public.ecr.aws/docker/library/alpine:3.16"
 
 set -eux -o pipefail
 
-NAME="test" exec_lxc sudo nerdctl pull --quiet $ALPINE_IMAGE
-NAME="test" exec_lxc nerdctl pull --quiet $ALPINE_IMAGE
+IMAGE_NAME="iperf3"
+
+NAME="test" exec_lxc systemctl --user restart containerd
+sleep 1
+NAME="test" exec_lxc systemctl --user restart buildkit
+sleep 3
+NAME="test" exec_lxc systemctl --user status --no-pager containerd
+NAME="test" exec_lxc systemctl --user status --no-pager buildkit
+NAME="test" exec_lxc /bin/bash -c "cd /home/ubuntu/bypass4netns/benchmark/iperf3 && sudo nerdctl build -f ./Dockerfile -t $IMAGE_NAME ."
+NAME="test" exec_lxc /bin/bash -c "cd /home/ubuntu/bypass4netns/benchmark/iperf3 && nerdctl build -f ./Dockerfile -t $IMAGE_NAME ."
 
 sudo lxc stop test
 sudo lxc copy test test2
@@ -30,12 +37,10 @@ TEST2_ADDR=$(sudo lxc exec test2 -- hostname -I | sed 's/ //')
 
 echo "===== Benchmark: iperf3 rootful with multinode via VXLAN ====="
 (
-  NAME="test" exec_lxc /bin/bash -c "sleep 3 && sudo nerdctl run -p 4789:4789/udp --privileged -d --name iperf3-server $ALPINE_IMAGE sleep infinity"
-  NAME="test" exec_lxc sudo nerdctl exec iperf3-server apk add --no-cache iperf3
+  NAME="test" exec_lxc /bin/bash -c "sleep 3 && sudo nerdctl run -p 4789:4789/udp --privileged -d --name iperf3-server $IMAGE_NAME"
   NAME="test" exec_lxc sudo /home/ubuntu/bypass4netns/test/setup_vxlan.sh iperf3-server $TEST1_VXLAN_MAC $TEST1_VXLAN_ADDR $TEST2_ADDR $TEST2_VXLAN_MAC $TEST2_VXLAN_ADDR
-  NAME="test2" exec_lxc /bin/bash -c "sleep 3 && sudo nerdctl run -p 4789:4789/udp --privileged -d --name iperf3-client $ALPINE_IMAGE sleep infinity"
+  NAME="test2" exec_lxc /bin/bash -c "sleep 3 && sudo nerdctl run -p 4789:4789/udp --privileged -d --name iperf3-client $IMAGE_NAME"
   NAME="test2" exec_lxc sudo /home/ubuntu/bypass4netns/test/setup_vxlan.sh iperf3-client $TEST2_VXLAN_MAC $TEST2_VXLAN_ADDR $TEST_ADDR $TEST1_VXLAN_MAC $TEST1_VXLAN_ADDR
-  NAME="test2" exec_lxc sudo nerdctl exec iperf3-client apk add --no-cache iperf3
 
   NAME="test" exec_lxc systemd-run --user --unit iperf3-server sudo nerdctl exec iperf3-server iperf3 -s
   NAME="test2" exec_lxc sudo nerdctl exec iperf3-client iperf3 -c $TEST1_VXLAN_ADDR -i 0 --connect-timeout 1000 -J > iperf3-multinode-rootful.log
@@ -47,12 +52,10 @@ echo "===== Benchmark: iperf3 rootful with multinode via VXLAN ====="
 
 echo "===== Benchmark: iperf3 client(w/o bypass4netns) server(w/o bypass4netns) with multinode via VXLAN ====="
 (
-  NAME="test" exec_lxc /bin/bash -c "sleep 3 && nerdctl run -p 4789:4789/udp --privileged -d --name iperf3-server $ALPINE_IMAGE sleep infinity"
-  NAME="test" exec_lxc nerdctl exec iperf3-server apk add --no-cache iperf3
+  NAME="test" exec_lxc /bin/bash -c "sleep 3 && nerdctl run -p 4789:4789/udp --privileged -d --name iperf3-server $IMAGE_NAME"
   NAME="test" exec_lxc /home/ubuntu/bypass4netns/test/setup_vxlan.sh iperf3-server $TEST1_VXLAN_MAC $TEST1_VXLAN_ADDR $TEST2_ADDR $TEST2_VXLAN_MAC $TEST2_VXLAN_ADDR
-  NAME="test2" exec_lxc /bin/bash -c "sleep 3 && nerdctl run -p 4789:4789/udp --privileged -d --name iperf3-client $ALPINE_IMAGE sleep infinity"
+  NAME="test2" exec_lxc /bin/bash -c "sleep 3 && nerdctl run -p 4789:4789/udp --privileged -d --name iperf3-client $IMAGE_NAME"
   NAME="test2" exec_lxc /home/ubuntu/bypass4netns/test/setup_vxlan.sh iperf3-client $TEST2_VXLAN_MAC $TEST2_VXLAN_ADDR $TEST_ADDR $TEST1_VXLAN_MAC $TEST1_VXLAN_ADDR
-  NAME="test2" exec_lxc nerdctl exec iperf3-client apk add --no-cache iperf3
 
   NAME="test" exec_lxc systemd-run --user --unit iperf3-server nerdctl exec iperf3-server iperf3 -s
   NAME="test2" exec_lxc nerdctl exec iperf3-client iperf3 -c $TEST1_VXLAN_ADDR -i 0 --connect-timeout 1000 -J > iperf3-multinode-wo-b4ns.log
@@ -67,10 +70,8 @@ echo "===== Benchmark: iperf3 client(w/ bypass4netns) server(w/ bypass4netns) wi
   NAME="test" exec_lxc systemd-run --user --unit etcd.service /usr/bin/etcd --listen-client-urls http://$TEST_ADDR:2379 --advertise-client-urls http://$TEST_ADDR:2379
   NAME="test" exec_lxc systemd-run --user --unit run-bypass4netnsd bypass4netnsd --multinode=true --multinode-etcd-address=http://$TEST_ADDR:2379 --multinode-host-address=$TEST_ADDR
   NAME="test2" exec_lxc systemd-run --user --unit run-bypass4netnsd bypass4netnsd --multinode=true --multinode-etcd-address=http://$TEST_ADDR:2379 --multinode-host-address=$TEST2_ADDR
-  NAME="test" exec_lxc /bin/bash -c "sleep 3 && nerdctl run --label nerdctl/bypass4netns=true -d -p 5202:5201 --name iperf3-server $ALPINE_IMAGE sleep infinity"
-  NAME="test" exec_lxc nerdctl exec iperf3-server apk add --no-cache iperf3
-  NAME="test2" exec_lxc /bin/bash -c "sleep 3 && nerdctl run --label nerdctl/bypass4netns=true -d --name iperf3-client $ALPINE_IMAGE sleep infinity"
-  NAME="test2" exec_lxc nerdctl exec iperf3-client apk add --no-cache iperf3
+  NAME="test" exec_lxc /bin/bash -c "sleep 3 && nerdctl run --label nerdctl/bypass4netns=true -d -p 5202:5201 --name iperf3-server $IMAGE_NAME sleep infinity"
+  NAME="test2" exec_lxc /bin/bash -c "sleep 3 && nerdctl run --label nerdctl/bypass4netns=true -d --name iperf3-client $IMAGE_NAME sleep infinity"
 
   SERVER_IP=$(NAME="test" exec_lxc nerdctl exec iperf3-server hostname -i)
   NAME="test" exec_lxc systemd-run --user --unit iperf3-server nerdctl exec iperf3-server iperf3 -s
